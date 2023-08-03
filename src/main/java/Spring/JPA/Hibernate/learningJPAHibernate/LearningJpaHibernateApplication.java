4642d06abb73117a4da6452d4f5784f0a8bb428c
Manikrishna Sanganabatla <msanganabatla@csu.fullerton.edu>
2023-08-03 14:42:53 -0700

    connected with H2 Base using Spring JDBC and started running a query

---
+++ src/main/java/Spring/JPA/Hibernate/learningJPAHibernate/LearningJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package Spring.JPA.Hibernate.learningJPAHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningJpaHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Spring/JPA/Hibernate/learningJPAHibernate/LearningJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring.JPA.Hibernate.learningJPAHibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
