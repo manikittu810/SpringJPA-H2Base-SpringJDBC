a15b35bcf2adf6cd6394e428ecbb7c9c8c344236
Manikrishna Sanganabatla <msanganabatla@csu.fullerton.edu>
2023-08-05 10:47:42 -0700

    added two methods in interface to call and print  result using author and name

--- src/main/java/Spring/JPA/Hibernate/learningJPAHibernate/course/springdatajpa/CourseSpringDataJPARepository.java
+++ src/main/java/Spring/JPA/Hibernate/learningJPAHibernate/course/springdatajpa/CourseSpringDataJPARepository.java
@@ -3,7 +3,11 @@ package Spring.JPA.Hibernate.learningJPAHibernate.course.springdatajpa;
 import Spring.JPA.Hibernate.learningJPAHibernate.course.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseSpringDataJPARepository extends JpaRepository<Course,Long > {
 //just creating interface the spring data jpa makes the jpa way more simple.
+    List<Course> findByAuthor(String author);
+    List<Course> findByName(String name);
 
 }
